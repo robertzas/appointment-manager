4bf65bec2c7dbdf75b217766081b17953461d217
Dina Steeve <dsteeve@knowledgefactor.com>
2015-04-27 09:18:10 -0600

    add in some pseudo code

--- src/main/resources/db/sprocs/sp_findNextTime.sql
+++ src/main/resources/db/sprocs/sp_findNextTime.sql
@@ -10,7 +10,16 @@ create or replace PROCEDURE findNextTime (employee_id NUMBER, service_id NUMBER,
       DBMS_OUTPUT.PUT_LINE('Time to do service is ' || timeRequired);
       DBMS_OUTPUT.PUT_LINE('end time ' || endTimestamp  );
       DBMS_OUTPUT.PUT_LINE('beg time ' || beginTimestamp  );
+
+
+--calculate appointmentEndTime = beginTimestamp + requiredTime;
+--if appointmentEndTime < givenTimestamp then
+--select * from reservation where beginTimestamp + timerequred is between reservationBegin and reservationEnd 
+--and reservation.employee_id = employee_id  
+--else return "no appt time available"
+--endif
+--if no reservations found, then good to go time is available.
+--if reservation is found, then take then endTimestamp of the reservation
+--loop through again with appointmentEndTime = newEndTimestamp + requiredTime;
      
-      
-      
    END;
