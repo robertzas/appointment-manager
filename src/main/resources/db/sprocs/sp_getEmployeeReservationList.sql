6cbb1847173132011a4b71572eb1c01872d091d7
Dina Steeve <dsteeve@knowledgefactor.com>
2015-05-01 09:13:03 -0600

    employee reservation

---
+++ src/main/resources/db/sprocs/sp_getEmployeeReservationList.sql
@@ -0,0 +1,21 @@
+create or replace procedure getEmployeeReservationList
+(fromdatetime IN timestamp, todatetime IN timestamp, employeeid IN number, emp_cursor out SYS_REFCURSOR )
+as
+
+fromtime timestamp := fromdatetime;
+totime timestamp  := todatetime;
+
+BEGIN
+
+  if (todatetime is null) then 
+    totime := sysdate + 7;
+    fromtime := sysdate;
+  end if;
+
+open emp_cursor for 
+SELECT r.*
+      from reservation r
+      where r.employee_service_employee_fk = employeeid
+      and r.start_timestamp between fromtime and totime;
+
+END;
