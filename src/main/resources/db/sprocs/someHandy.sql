fedfae01cfb33df8ef8a9fc658ac929d272a2560
Dina Steeve <dsteeve@knowledgefactor.com>
2015-05-06 21:36:36 -0600

    updated sql for presentation

--- src/main/resources/db/sprocs/someHandy.sql
+++ src/main/resources/db/sprocs/someHandy.sql
@@ -1,15 +1,6 @@
 --set the serverouput on.
 SET SERVEROUTPUT ON;
 
---this will execute sickCall sproc with a given date.
-execute sickCall(10001, to_timestamp('2015-05-04','YYYY-MM-DD'));
-
---this will back out those changes so you can run it again
-update reservation set is_cancelled = 0
-where is_cancelled = 1;
-delete from cancellation where 1=1;
-commit;
-
 
 --call FIND NEXT TIME
 --10001 is the employee we want
@@ -35,6 +26,30 @@ BEGIN
   dbms_output.put_line('reservationNumber: ' || reservationNumber);
  
 END;
+
+
+
+--this will execute sickCall sproc with a given date.
+execute sickCall(10001, to_timestamp('2015-05-04','YYYY-MM-DD'));
+
+--this will back out those changes so you can run it again
+update reservation set is_cancelled = 0
+where is_cancelled = 1;
+delete from cancellation where 1=1;
+commit;
+
+
+
+--Get the Employee's schedule
+variable rc refcursor;
+exec getEmployeeReservationList(to_timestamp('2015-04-01', 'YYYY-MM-DD'),to_timestamp('2015-04-30' ,'YYYY-MM-DD'),10001, :rc);
+print rc;
+
+--Get the Customer's History
+variable rc refcursor;
+exec getCustomerHistory(to_timestamp('2015-01-01', 'YYYY-MM-DD'),to_timestamp('2015-04-30' ,'YYYY-MM-DD'),1000, :rc);
+print rc;
+
 
 
 --we should probably do error checking, but we don't! 
